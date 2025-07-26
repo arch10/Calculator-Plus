b357b699306b2347827b5afe9aa333110c052c33
Archit <arch10@ymail.com>
2025-07-27 02:42:32 +0530

    refactor: remove unused onCreate method in AboutActivity

--- app/src/main/java/com/gigaworks/tech/calculator/ui/about/AboutActivity.kt
+++ app/src/main/java/com/gigaworks/tech/calculator/ui/about/AboutActivity.kt
@@ -1,15 +1,10 @@
 package com.gigaworks.tech.calculator.ui.about
 
-import android.os.Bundle
 import android.view.LayoutInflater
 import com.gigaworks.tech.calculator.databinding.ActivityAboutBinding
 import com.gigaworks.tech.calculator.ui.base.BaseActivity
 
 class AboutActivity : BaseActivity<ActivityAboutBinding>() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
 
     override fun getViewBinding(inflater: LayoutInflater) = ActivityAboutBinding.inflate(inflater)
 }
